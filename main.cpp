4de0771d859b6399927e981609e4772e46e4a1a9
TFS11 <cakeivanov@mail.ru>
2023-09-19 15:19:56 +0300

    add player_turn function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-
-int main() {
-
-    return 0;
+char** player_turn(char** GF){
+    int i,j;
+    bool flag= false;
+    std :: cout << "Enter coordinates of cross" << std:: endl;
+    while(!flag) {
+        std:: cin >> i >> j;
+        if (i >= 0 && i <= 2 && j >= 0 && j <= 2 && GF[i][j] == ' ') {
+            GF[i][j] = 'X';
+            flag= true;
+        } else { std:: cout << "Enter another coordinates idiot!!!"<< std:: endl; }
+    }
+    return GF;
 }
